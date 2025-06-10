0d4024377847a4105a621b0ca6b390c060edb934
MK <mk@MKAir.local>
2025-06-10 22:34:16 +0200

    Update geocoding script for 1,000-row dataset and deploy recent UI changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+ import { defineConfig } from 'vite';
+ import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/claim-map/', // This line is key
-  plugins: [react()],
-});
+ // https://vitejs.dev/config/
+ export default defineConfig({
+   base: '/claim-map/',
+   plugins: [react()],
+   build: {
+     outDir: 'docs'
+   }
+ });
